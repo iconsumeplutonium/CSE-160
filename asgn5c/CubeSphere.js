92aadf7124715104502f129b2b8908d98454d750
iconsumeplutonium <uzymc242@outlook.com>
2024-06-01 23:15:41 -0700

    cube sphere is a failure, implemented marching cubes instead, noise doesnt work with offsets

--- asgn5c/CubeSphere.js
+++ asgn5c/CubeSphere.js
@@ -12,25 +12,27 @@ export function createSquare(resolution, localUp, radius, normalMap) {
     let triangles = new Uint32Array((resolution - 1) * (resolution - 1) * 6);
     let triIndex = 0;
 
-    const noiseScale = 0.5
+    let normals = new Float32Array(resolution * resolution * 3);
+    let colorData = [];
+    
     for (let y = 0; y < resolution; y++) {
         for (let x = 0; x < resolution; x++) {
             let i = x + y * resolution;
             let percent = new three.Vector2(x, y).divideScalar(resolution - 1);
             let pointOnUnitCube = localUp.clone().add(axisA.clone().multiplyScalar((percent.x - 0.5) * 2)).add(axisB.clone().multiplyScalar((percent.y - 0.5) * 2));
-
             let pointOnUnitSphere = pointOnUnitCube.normalize();
+            normals.set([pointOnUnitSphere.x, pointOnUnitSphere.y, pointOnUnitSphere.z], i * 3);
 
-            let height = (Noise.FBM(noiseScale, 8, 0.5, 2, pointOnUnitSphere) + 1) / 2;
+            let height = getHeight(pointOnUnitSphere);
+            let color = getColor(height);
+            colorData.push(color.x, color.y, color.z, 255);
 
-            let idk = noise.simplex3(pointOnUnitSphere.x, pointOnUnitSphere.y, pointOnUnitSphere.z);
-            height = three.MathUtils.lerp(height, 0, idk)
-            //height = bias2(height)
 
             pointOnUnitSphere.multiplyScalar(radius + height)
             vertices.set([pointOnUnitSphere.x, pointOnUnitSphere.y, pointOnUnitSphere.z], i * 3);
 
             uvs.set([percent.x, percent.y], i * 2);
+            //normals.set([])
 
             if (x != resolution - 1 && y != resolution - 1) {
                 triangles[triIndex] = i;
@@ -45,24 +47,56 @@ export function createSquare(resolution, localUp, radius, normalMap) {
         }
     }
 
-
-
-
+    const texture = new three.DataTexture(new Uint8Array(colorData), resolution, resolution, three.RGBAFormat, three.UnsignedByteType);
+    texture.needsUpdate = true;
 
     const geometry = new three.BufferGeometry();
     geometry.setAttribute('position', new three.BufferAttribute(vertices, 3));
     geometry.setIndex(new three.BufferAttribute(triangles, 1));
     geometry.setAttribute('uv', new three.BufferAttribute(uvs, 2));
+    //geometry.setAttribute('normal', new three.BufferAttribute(normals, 3));
     geometry.computeVertexNormals()
-    geometry.computeTangents()
+    geometry.computeTangents();
 
     const material = new three.MeshPhongMaterial({ 
         wireframe: false,
+        flatShading: false,
+        map: texture
         // normalMap: normalMap,
         // normalScale: new three.Vector2(0.1, 0.1),
         // color: 0xaaa7a6
     });
 
+    // const material = new three.ShaderMaterial({
+    //     uniforms: {
+    //         u_LightPos: { value: new three.Vector3(1, 1, 1) }, // Set your light position here
+    //         u_FragColor: { value: new three.Color(1, 1, 1) } // Set your fragment color here
+    //     },
+    //     vertexShader: `
+    //         varying vec3 v_Position;
+    //         varying vec3 v_Normal;
+    //         void main() {
+    //             v_Position = position;
+    //             v_Normal = normalize(normalMatrix * normal);
+    //             gl_Position = projectionMatrix * modelViewMatrix * vec4(position, 1.0);
+    //         }
+    //     `,
+    //     fragmentShader: `
+    //         uniform vec3 u_LightPos;
+    //         uniform vec3 u_FragColor;
+    //         varying vec3 v_Position;
+    //         varying vec3 v_Normal;
+    //         void main() {
+    //             vec3 L = normalize(u_LightPos - v_Position);
+    //             vec3 N = normalize(v_Normal);
+    //             vec3 k_d = vec3(0.5, 0.5, 0.5);
+    //             vec3 diffuse = k_d * max(dot(L, N), 0.0) * u_FragColor;
+    //             gl_FragColor = vec4(v_Normal, 1.0);
+    //         }
+    //     `
+    // });
+    
+    
     
     // material.bumpMap = normalMap;
     // console.log(material.bumpMap)
@@ -78,6 +112,35 @@ export function createSquare(resolution, localUp, radius, normalMap) {
     
 }
 
+function getHeight(point) {
+    const noiseScale = 0.5
+    let height = (Noise.FBM(noiseScale, 8, 0.5, 2, point) + 1) / 2;
+
+    let idk = noise.simplex3(point.x, point.y, point.z);     //   ((Noise.FBM(2, 8, 0.5, 2, pointOnUnitSphere) + 1) / 2)
+    //console.log(idk)
+    //height = bias2(height)
+
+    height = three.MathUtils.lerp(0, height, three.MathUtils.clamp(idk, 0, 1))
+
+    // if (height < 0.2)
+    //     height = three.MathUtils.lerp(height, 0, idk);
+
+    return height;
+}
+
+function getColor(x) {
+
+        if (x <= 0.0)
+            return new three.Vector3(0, 0, 255);
+        else if (x > 0 && x < 0.2)
+            return new three.Vector3(219, 234, 167);
+        else if (x >= 0.5 && x < 0.75)
+            return new three.Vector3(0, 255, 0);
+        else 
+            return new three.Vector3(51, 53, 58);
+
+}
+
 function bias(x) {
     const bias = 0.55;
     const k = Math.pow(1 - bias, 3);
@@ -89,14 +152,25 @@ function bias2(x) {
     return Math.exp(4 * (x - 1));
 }
 
+function sigmoid(x) {
+    //const b = 0.59;
+
+    //if (x <= b)
+        return 1 / (1 + Math.exp(-4 * (x - 0.75)));
+    //else
+        //return x - 0.25;
+}
+
 
 export class CubeSphere {
     constructor(position, s1, scene, normalMap) {
         this.meshes = [];
         const faceResolution = 500;
         const radius = 10;
+
+        noise.seed(0);
         
-        let topFace = createSquare(faceResolution, new three.Vector3(0, 1, 0), radius, normalMap);
+        let topFace = createSquare(faceResolution , new three.Vector3(0, 1, 0), radius, normalMap);
         this.meshes.push(topFace);
 
         let bottomFace = createSquare(faceResolution, new three.Vector3(0, -1, 0), radius, normalMap);
@@ -108,7 +182,7 @@ export class CubeSphere {
         let backFace = createSquare(faceResolution, new three.Vector3(0, 0, -1), radius, normalMap);
         this.meshes.push(backFace);
 
-        let leftFace = createSquare(faceResolution, new three.Vector3(1, 0, 0), radius, normalMap);
+        let leftFace = createSquare(faceResolution , new three.Vector3(1, 0, 0), radius, normalMap);
         this.meshes.push(leftFace);
         
         let rightFace = createSquare(faceResolution, new three.Vector3(-1, 0, 0), radius, normalMap);
--- asgn5c/asgn5c.js
+++ asgn5c/asgn5c.js
@@ -2,6 +2,7 @@ import * as three from 'three';
 import * as PlayerController from './PlayerController.js';
 import * as CubeSphere from './CubeSphere.js';
 import * as UIManager from './UIManager.js';
+import * as LookupTable from './LookupTable.js';
 import {OBJLoader} from 'three/addons/loaders/OBJLoader.js';
 import {MTLLoader} from 'three/addons/loaders/MTLLoader.js';
 import { OrbitControls } from 'three/addons/controls/OrbitControls.js';
@@ -50,11 +51,11 @@ function main() {
     // let light = new three.PointLight(0xFFFFFF, 10, 0, 2);
     // light.position.set(0, 20, 0);
     // scene.add(light);asd
-    let dirLight = new three.DirectionalLight( 0xffffff, 3 );
+    let dirLight = new three.DirectionalLight( 0xffffff, 1.5);
     dirLight.position.set(1, 9999999, 1)
     scene.add( dirLight );
 
-    ;let light = new three.AmbientLight(0xFFFFFF, 0.1);
+    let light = new three.AmbientLight(0xFFFFFF, 1);
     scene.add(light);
 
     controls = new PointerLockControls(camera, document.body);
@@ -76,8 +77,10 @@ function main() {
         normalMap = texture;
         normalMap.colorSpace = three.SRGBColorSpace;
         normalMap.minFilter = three.LinearMipmapLinearFilter;
-        console.log(normalMap)
-        c = new CubeSphere.CubeSphere(new three.Vector3(0, 0, 0), 100, scene, normalMap)
+        //console.log(normalMap)
+        //c = new CubeSphere.CubeSphere(new three.Vector3(0, 0, 0), 100, scene, normalMap)
+        marchingCubes(new three.Vector3(0, 0, 0))
+        marchingCubes(new three.Vector3(-20, 0, 0))
         requestAnimationFrame(Update);
     })
 }
@@ -96,12 +99,134 @@ function Update(time) {
 
     // //c = new CubeSphere.CubeSphere(new three.Vector3(0, 0, 0), 100, scene, slider1.value / 100)
     // c.makeMeshes(100, 1, slider1.value / 100, scene)
-    c.setWireframeVisibility(UIManager.wireframeViewCheckbox.checked);
+    //c.setWireframeVisibility(UIManager.wireframeViewCheckbox.checked);
 
     renderer.render(scene, camera);
     requestAnimationFrame(Update);
 }
 
+
+const surfaceLevel = 0.5;
+const size = 20;
+const offset = Math.random();
+function marchingCubes(startPos) {
+    let allVerts = [];
+    noise.seed(0)
+    for (let i = startPos.x; i < 40 + startPos.x; i++) {
+        for (let j = startPos.y; j < size + startPos.y; j++) {
+            for (let k = startPos.z; k < size + startPos.z; k++) {
+                //let sample = noise.simplex3(i + offset, j + offset, k + offset)
+                let sample = Noise.FBM(40, 8, 0.5, 2, new three.Vector3(i + offset, j + offset, k + offset));
+                //console.log(i, j, k, sample)
+                allVerts.push(new three.Vector4(i, j, k, sample));
+            }
+        }
+    }
+
+    // allVerts.push(new three.Vector4(0, 0, 0, 0));
+    // allVerts.push(new three.Vector4(0, 0, 1, 0));
+    // allVerts.push(new three.Vector4(0, 1, 0, 0));
+    // allVerts.push(new three.Vector4(0, 1, 1, 1));
+    // allVerts.push(new three.Vector4(1, 0, 0, 0));
+    // allVerts.push(new three.Vector4(1, 0, 1, 0));
+    // allVerts.push(new three.Vector4(1, 1, 0, 0));
+    // allVerts.push(new three.Vector4(1, 1, 1, 0));
+
+    const geometry = new three.BufferGeometry();
+    let verts = [];
+    for (let i = 0; i < size - 1; i++) {
+        for (let j = 0; j < size - 1; j++) {
+            for (let k = 0; k < size - 1; k++) {
+
+                let corners = [];
+                corners.push(coordToIndex(i,     j,     k    ));
+                corners.push(coordToIndex(i + 1, j,     k    ));
+                corners.push(coordToIndex(i + 1, j,     k + 1));
+                corners.push(coordToIndex(i,     j,     k + 1));
+                corners.push(coordToIndex(i,     j + 1, k    ));
+                corners.push(coordToIndex(i + 1, j + 1, k    ));
+                corners.push(coordToIndex(i + 1, j + 1, k + 1));
+                corners.push(coordToIndex(i,     j + 1, k + 1));
+
+                //calculate halfway vectors for each edge, based on https://paulbourke.net/geometry/polygonise/polygonise1.gif
+                let edges = [];
+                edges.push(new three.Vector3(i + 0.5, j,       k      ));
+                edges.push(new three.Vector3(i + 1,   j,       k + 0.5));
+                edges.push(new three.Vector3(i + 0.5, j,       k + 1  ));
+                edges.push(new three.Vector3(i,       j,       k + 0.5));
+                edges.push(new three.Vector3(i + 0.5, j + 1,   k      ));
+                edges.push(new three.Vector3(i + 1,   j + 1,   k + 0.5));
+                edges.push(new three.Vector3(i + 0.5, j + 1,   k + 1  ));
+                edges.push(new three.Vector3(i,       j + 1,   k + 0.5));
+                edges.push(new three.Vector3(i,       j + 0.5, k      ));
+                edges.push(new three.Vector3(i + 1,   j + 0.5, k      ));
+                edges.push(new three.Vector3(i + 1,   j + 0.5, k + 1  ));
+                edges.push(new three.Vector3(i,       j + 0.5, k + 1  ));
+                
+                let index = 0;
+                for (let p = 0; p < 8; p++) {
+                    //console.log(allVerts[corners[p]])
+                    if (allVerts[corners[p]].w <= surfaceLevel)
+                        index |= Math.pow(2, p);
+                }
+
+                // let activeEdgesHex = LookupTable.edgeTable[index];
+                // let activeEdges = LookupTable.getActiveEdges(activeEdgesHex);
+
+                //console.log(index)
+
+                let activeTris = LookupTable.triTable[index];
+                for (let x = 0; activeTris[x] != -1; x += 3) {
+                    let p1 = edges[activeTris[x]];
+                    let p2 = edges[activeTris[x + 1]];
+                    let p3 = edges[activeTris[x + 2]];
+                    
+                    verts.push(p1.x + startPos.x, p1.y + startPos.y, p1.z + startPos.z, p2.x + startPos.x, p2.y + startPos.y, p2.z + startPos.z, p3.x + startPos.x, p3.y + startPos.y, p3.z + startPos.z);
+                }
+            }
+        }
+    }
+
+    //console.log(verts)
+
+    geometry.setAttribute('position', new three.BufferAttribute(new Float32Array(verts), 3));
+
+    // for (let i = 0; i < allVerts.length; i++) {
+    //     // let sample = (noise.simplex3(i + offset, j + offset, k + offset) + 1) / 2;
+    //     // if (sample > surfaceLevel)
+    //     //     continue;
+    //     if (allVerts[i].w > surfaceLevel)
+    //         continue;
+    //     //const col = (sample < surfaceLevel) ? 0x0000FF : 0xFFFFFF;
+
+    //     //const geo = new three.BoxGeometry(1, 1, 1);
+    //     const geo = new three.SphereGeometry(0.1, 10, 10);
+    //     const mat = new three.MeshPhongMaterial({
+    //         color: 0xFFFFFF
+    //     });
+
+    //     const m = new three.Mesh(geo, mat);
+    //     m.position.x =  allVerts[i].x;
+    //     m.position.y =  allVerts[i].y;
+    //     m.position.z =  allVerts[i].z;
+    //     scene.add(m);
+            
+    // }
+
+    geometry.computeVertexNormals()
+    const material = new three.MeshPhongMaterial({
+        color: 0xFFFFFF * Math.random(),
+        side: three.DoubleSide
+    });
+    const mesh = new three.Mesh(geometry, material);
+
+
+    scene.add(mesh);
+}
+
+function coordToIndex(x, y, z) {
+    return (z * size * size) + (y * size) + x;
+}
 
 
 
